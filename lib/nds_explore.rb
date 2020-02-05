956ff4d6547c28cc3d0b42351ec27dbad0a1a94e
Asad Jafri <asadhjafri@gmail.com>
2020-02-05 19:54:30 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,7 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'pp'
 require 'directors_database'
-# Call the method directors_database to retrieve the NDS
+pp directors_database# Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
